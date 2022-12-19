00eb4003d1178074a23b34f9617847d482053f30
Kyle Jung <ksjung1@uci.edu>
2022-12-19 01:37:30 -0800

    Update build for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import LocationFinder from './LocationFinder';
 
 function App() {
 	const postalCodeRef = useRef()
-	var textArray = ["", "", ""]
 	// handles the code when the user searches a zip code
 	function handlePostalCode() {
 		const postalCode =  postalCodeRef.current.value
@@ -36,43 +35,40 @@ function App() {
 		const printResult = () => {
 			result.then((animalArray) => {
 				const nearbyPets = animalArray.animals
-				var counter = 0;
+				// for loop that iterates up to 9 animals in the user's vicinity
 				for (let i = 0; i < nearbyPets.length; i++) {
-					textArray[counter] += nearbyPets[i].name + "<br>";
-					if (counter >= 2) {
-						counter = 0
-					}
-					else {
-						counter += 1
-					}
-					// facing issue with extracting the image from nearbyPets
-					console.log(nearbyPets[i].photos[0])
-                	var img = document.createElement('img');
-            		img.src = nearbyPets[i].photos[0] + ".png";
-            		document.body.appendChild(img);
+					document.getElementById(String(i+1)).innerHTML = nearbyPets[i].name + "<br>";
+					// create an img element to display an image of the available pet
+					var img = document.createElement('img');
+					img.src = nearbyPets[i].photos[0].full;
+					document.getElementById(String(i+1)).appendChild(img);
 				}
-				document.getElementById("column1").innerHTML = textArray[0];
-				document.getElementById("column2").innerHTML = textArray[1];
-				document.getElementById("column3").innerHTML = textArray[2];
-				// comment below is a way to clear our page to display information about pets
-				// document.getElementById("header").innerHTML = "";
 			});
 		};
 		printResult();
+		// comment below is a way to clear our page to display information about pets
+		document.getElementById("header").innerHTML = "";
 	}
 	return (
 	<>
 		<div className="App">
-			<div className="row">
-				<p id="column1" className="column"></p>
-				<p id="column2" className="column"></p>
-				<p id="column3" className="column"></p>
+			{/* grid element that holds the name and adjusted image of an available pet */}
+			<div className="grid-container">
+				<div id="1" className="grid-item"></div>
+				<div id="2" className="grid-item"></div>
+				<div id="3" className="grid-item"></div>
+				<div id="4" className="grid-item"></div>
+				<div id="5" className="grid-item"></div>
+				<div id="6" className="grid-item"></div>
+				<div id="7" className="grid-item"></div>
+				<div id="8" className="grid-item"></div>
+				<div id="9" className="grid-item"></div>
 			</div>
 			<header id="header" className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
+				<img src={logo} className="App-logo" alt="logo" /><br/>
 				<LocationFinder />
 				<input ref={postalCodeRef} type="text" />
-				<button id="searchButton" onClick={handlePostalCode}>Search</button><br />
+				<button id="searchButton" onClick={handlePostalCode}>Search</button><br/>
 				<a className="App-link" href="https://www.petfinder.com" target="_blank" rel="noopener noreferrer">Petfinder</a>
 			</header>
 		</div>
