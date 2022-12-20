f60694243f270990d4883f22fa298c0c943bb10b
Kyle Jung <ksjung1@uci.edu>
2022-12-19 23:06:37 -0800

    Update build to GitHub Pages

--- src/Instructions.js
+++ src/Instructions.js
@@ -4,8 +4,8 @@ export default function Instructions() {
   return (
     <div>
         Welcome to Pet Roulette!<br/>
-        36 pets available for adoption will be presented in the location's vicinity.<br/>
-        Good luck with your new pet!
+        Find and adopt your pet among 36 available in the location's vicinity.<br/>
+        Good luck on on the jackpot!
     </div>
   )
 }
